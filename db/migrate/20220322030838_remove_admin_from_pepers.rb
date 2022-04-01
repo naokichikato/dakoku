67e641dfb666aa2005249e7a6fbe893efaa73a9c
naokichikato <katonaokichi3@gmail.com>
2022-04-01 12:41:40 +0900

    6 データ削除

--- db/migrate/20220322030838_remove_admin_from_pepers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAdminFromPepers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :pepers, :admin, :boolean
-  end
-end
--- db/migrate/20220322040324_rename_type_column_to_pepers.rb
+++
@@ -1,5 +0,0 @@
-class RenameTypeColumnToPepers < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :pepers, :type, :kind
-  end
-end
